57f52d0a73b0467a1fe36cbaa2a49464105c3ce8
natyrabajgora <natyrabajgora11@gmail.com>
2025-05-26 21:29:50 +0200

    Permirsime

--- Sherbime/database.php
+++ Sherbime/database.php
@@ -7,6 +7,6 @@ $dbname = "stafi";
 $con = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$con) {
-    echo "Gabim lidhja me databazën: " . mysqli_connect_error();
+    die("Gabim lidhja me databazën: " . mysqli_connect_error());
 }
 ?>
--- Sherbime/insertStafi.php
+++ Sherbime/insertStafi.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'database.php';
-require_once 'stafi.php'; // KU KRIJON $doctor1, $doctor2, etj.
+require_once 'krijoStafi.php'; // Ku krijohen objektet $doctor1, $doctor2, etj.
 
 $doctors = [$doctor1, $doctor2, $doctor3, $doctor4];
 
@@ -17,13 +17,7 @@ foreach ($doctors as $doctor) {
 
         $stmt = mysqli_prepare($con, $sql);
         mysqli_stmt_bind_param($stmt, "sssss", $name, $title, $description, $contact, $specialization);
-
-        if (mysqli_stmt_execute($stmt)) {
-            echo "Doktori $name u shtua me sukses.<br>";
-        } else {
-            echo "Gabim me doktorin $name: " . mysqli_error($con) . "<br>";
-        }
-
+        mysqli_stmt_execute($stmt);
         mysqli_stmt_close($stmt);
     }
 }
